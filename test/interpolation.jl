98037bd69d1249fe1069f1a0823ff9f4c3969339
Bart de Koning <bart.dekoning@deltares.nl>
2024-05-23 19:22:43 +0200

    Add tests for SmoothedLinearInterpolation

--- test/interpolation.jl
+++ test/interpolation.jl
@@ -1,10 +1,51 @@
 using SmoothInterpolation
+using Random
 
-# @testset "SmoothedLinearInterpolation" begin
-#     u = [1.0, 2.0, 3.0, 4.0]
-#     t = [1.0, 2.0, 3.0, 4.0]
+@testset "SmoothedLinearInterpolation degenerate" begin
+    Random.seed!(1)
 
-#     itp = SmoothedLinearInterpolation(u, t; extrapolate = true)
-#     @test itp(0.0) ≈ 0.0
-#     @test itp(5.0) ≈ 5.0
-# end
+    u = cumsum(rand(5))
+    t = [1.0, 2.0, 3.0, 4.0, 5.0]
+    itp = SmoothedLinearInterpolation(u, t)
+
+    @test all(itp.cache.ΔΔt .≈ 0)
+    @test itp.(1.5:0.3:5.0) ≈ [
+        0.24799,
+        0.35276,
+        0.49293,
+        0.70214,
+        0.91179,
+        1.11923,
+        1.30991,
+        1.49839,
+        1.68723,
+        1.93269,
+        2.20716,
+        2.48164,
+    ] atol = 1e-4
+end
+
+@testset "SmoothedLinearInterpolation non-degenerate" begin
+    Random.seed!(2)
+    u = cumsum(rand(5))
+    t = cumsum(rand(5) .+ (1:5))
+    itp = SmoothedLinearInterpolation(u, t)
+
+    @test !any(itp.cache.ΔΔt[2:(end - 1)] .≈ 0)
+    @test itp.(t[1]:1.2:t[end]) ≈ [
+        0.00225,
+        0.30983,
+        0.61718,
+        0.88465,
+        1.14094,
+        1.39383,
+        1.60492,
+        1.80825,
+        2.01157,
+        2.19797,
+        2.27307,
+        2.32361,
+        2.37415,
+        2.42469,
+    ] atol = 1e-4
+end
